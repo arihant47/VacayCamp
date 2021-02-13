98b323b21b3d6d9f4ec286a5624988f9cb6b757a
Arihant Kharai <kharaiarihant47@gmail.com>
2021-02-13 10:12:14 +0000

    Connect mongoose model  to app.js

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var express = require("express");
-var app = express();
 var path = require("path");
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var app = express();
+
+mongoose.connect("mongodb://localhost/vacay-camp", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+	console.log("Database Connected");
+});
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", function(req, res){
 	res.render("home");
+});
+
+app.get("/makecampground", async function(req, res){
+	var camp = new Campground({title: "My Backyard", description: "Cheap Camping!"});
+	await camp.save();
+	res.send(camp);
 });
 
 app.listen(3000, function(){
