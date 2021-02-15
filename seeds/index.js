7fe546e0345b9374ef9731333b9faa651018abce
Arihant Kharai <kharaiarihant47@gmail.com>
2021-02-15 09:46:11 +0000

    Seed the database

---
+++ seeds/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const Campground = require('../models/campground');
+
+mongoose.connect("mongodb://localhost/vacay-camp", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+	console.log("Database Connected");
+});
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
