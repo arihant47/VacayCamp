147db44e07fcb5d92345fe3ace77da2cc40aeba7
Arihant Kharai <kharaiarihant47@gmail.com>
2021-02-15 09:49:28 +0000

    Seeding Campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect("mongodb://localhost/vacay-camp", {
@@ -14,13 +15,16 @@ db.once("open", function() {
 	console.log("Database Connected");
 });
 
+var sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async () => {
+
+var seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        var random1000 = Math.floor(Math.random() * 1000);
+        var camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
     }
