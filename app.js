4aa4a1a4a91e3c6f616186e137fac0a7cc6d57ad
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-13 17:39:49 +0000

    Prevent error from Postman/ajax

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get("/campgrounds/new", function(req, res){
 });
 
 app.post("/campgrounds", catchAsync( async function(req, res, next){
+	if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
 	var campground = new Campground(req.body.campground);
 	await campground.save();
 	res.redirect(`/campgrounds/${campground._id}`);
