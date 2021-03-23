78cb320dc5d1036f9c9babf5813507fe69ed557d
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-23 17:53:02 +0000

    Adding mongoose middleware

--- models/campground.js
+++ models/campground.js
@@ -15,4 +15,8 @@ var CampgroundSchema = new Schema({
 	]
 });
 
+CampgroundSchema.post("findOneAndDelete", async function(){
+	console.log("Deleted!!");
+});
+
 module.exports = mongoose.model("Campground", CampgroundSchema);
