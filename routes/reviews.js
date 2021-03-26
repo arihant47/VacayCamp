ddad03e545e8189b94d5fd3abec80dfa627ecd17
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-26 04:56:54 +0000

    Relocate Review routes

---
+++ routes/reviews.js
@@ -0,0 +1,15 @@
+app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async function(req, res){
+	var campground = await Campground.findById(req.params.id);
+	var review = new Review(req.body.review);
+	campground.reviews.push(review);
+	await review.save();
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async function(req, res){
+	var {id, reviewId} = req.params;
+	await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+	await Review.findByIdAndDelete(reviewId);
+	res.redirect(`/campgrounds/${id}`);
+}));
