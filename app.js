debbf099365ebf9647af21570638038c8288fe9e
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-26 04:56:41 +0000

    Relocate Review routes

--- app.js
+++ app.js
@@ -48,22 +48,6 @@ app.get("/", function(req, res){
 	res.render("home");
 });
 
-app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async function(req, res){
-	var campground = await Campground.findById(req.params.id);
-	var review = new Review(req.body.review);
-	campground.reviews.push(review);
-	await review.save();
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async function(req, res){
-	var {id, reviewId} = req.params;
-	await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-	await Review.findByIdAndDelete(reviewId);
-	res.redirect(`/campgrounds/${id}`);
-}));
-
 app.all("*", function(req, res, next){
 	next(new ExpressError("Page not found!", 404))
 });
