6ec64572072c4bbdaf3501689ba07588e076e1c4
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-26 05:01:12 +0000

    Export Router

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/campgrounds/:id/reviews", validateReview, catchAsync(async function(req, res){
+router.post("/", validateReview, catchAsync(async function(req, res){
 	var campground = await Campground.findById(req.params.id);
 	var review = new Review(req.body.review);
 	campground.reviews.push(review);
@@ -10,9 +10,11 @@ router.post("/campgrounds/:id/reviews", validateReview, catchAsync(async functio
 	res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async function(req, res){
+router.delete("/:reviewId", catchAsync(async function(req, res){
 	var {id, reviewId} = req.params;
 	await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
 	await Review.findByIdAndDelete(reviewId);
 	res.redirect(`/campgrounds/${id}`);
 }));
+
+module.exports = router;
