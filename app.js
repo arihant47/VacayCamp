b875666bf9472a9b2fb1335821f59a3e537344e2
Arihant Kharai <kharaiarihant47@gmail.com>
2021-03-28 06:49:38 +0000

    Config Cookies

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ app.set('views', path.join(__dirname, 'views'));
 var sessionConfig = {
 	secret: 'thisisasecret!',
 	resave: false,
-	saveUninitialized: true
+	saveUninitialized: true,
+	cookie: {
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
 }
 app.use(session(sessionConfig))
 
