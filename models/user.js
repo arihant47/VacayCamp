bb80fe723feb970ea44a4fcdb563f7705b7bb3ae
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-01 07:14:48 +0000

    Define UserSchema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,11 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+	email: {
+		type: String,
+		required: true
+	}
+});
+UserSchema.plugin(passportLocalMongoose);
