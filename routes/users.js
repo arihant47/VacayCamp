3e82f756f4641cd0acced1088a76177c7bb7b645
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-02 10:29:53 +0000

    Define regester route

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,8 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user");
 
+router.get("/register", function(req,res){
+	res.render("users/register");
+});
+
 module.exports = router;
