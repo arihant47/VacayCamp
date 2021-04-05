1d7415630c858264dc4c2e0d0c2422045ad00c3e
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-05 06:34:57 +0000

    Require catchAsync

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
 var express = require("express");
 var router = express.Router();
+var catchAsync = require("../utils/catchAsync");
 var User = require("../models/user");
 
 router.get("/register", function(req,res){
 	res.render("users/register");
 });
 
-router.post("/register", async function(req, res){
+router.post("/register", catchAsync(async function(req, res){
 	var{email, username, password} = req.body;
 	var user = new User({email, username});
 	var registeredUser = await User.register(user, password);
 	console.log(registeredUser);
 	req.flash("success", "Welcome to VacayCamp!");
 	res.redirect("/campgrounds");
-});
+}));
 
 module.exports = router;
