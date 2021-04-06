3dd21d6b390333c6872c1562f7ad985ce453fa7b
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-06 07:44:47 +0000

    Define isLoggedIn middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
+module.exports.isLoggedIn = function(req, res, next){
 	if(!req.isAuthenticated()){
-		req.flash("error", "You must be signed in!");
+		req.flash("error", "You must be signed in first!");
 		return res.redirect("/login");
 	}
+	next();
+}
