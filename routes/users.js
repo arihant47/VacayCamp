4b5dc8baf9883900ebf88bf99f8ba6166a6f690b
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-07 09:24:53 +0000

    Fix Register Route

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,16 @@ router.get("/register", function(req,res){
 	res.render("users/register");
 });
 
-router.post("/register", catchAsync(async function(req, res){
+router.post("/register", catchAsync(async function(req, res, next){
 	try{
 		var{email, username, password} = req.body;
 		var user = new User({email, username});
 		var registeredUser = await User.register(user, password);
-		req.flash("success", "Welcome to VacayCamp!");
-		res.redirect("/campgrounds");
+		req.login(registeredUser, function(err){
+			if(err) return next(err);
+			req.flash("success", "Welcome to VacayCamp!");
+			res.redirect("/campgrounds");
+		});
 	} catch(e) {
 		req.flash("error", e.message);
 		res.redirect("register");
