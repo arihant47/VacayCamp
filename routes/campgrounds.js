af94e19f734e460c5450131e1f44871a89610235
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-10 19:26:43 +0000

    Adding campground permissions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -45,10 +45,15 @@ router.get("/:id", catchAsync(async function(req, res){
 }));
 
 router.get("/:id/edit", isLoggedIn, catchAsync( async function(req, res){
-	var campground = await Campground.findById(req.params.id)
+	var {id} = req.params;
+	var campground = await Campground.findById(id)
 	if(!campground){
 		req.flash("error", "Cannot find that campground!");
 		return res.redirect("/campgrounds");
+	}
+	if(!campground.author.equals(req.user._id)){
+		req.flash("error", "You do not have permission to do that!");
+		return res.redirect(`/campgrounds/${id}`);
 	}
 	res.render("campgrounds/edit", {campground});
 }));
