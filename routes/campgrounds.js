90b1034e2edff172cbc99409378fc54a0917a55a
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-10 19:36:03 +0000

    Add Authorization Milldeware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,6 +16,16 @@ var validateCampground = (req, res, next) => {
 	}
 }
 
+var isAuthor = async function(req, res, next){
+	var {id}= req.params;
+	var campground = await Campground.findById(id);
+	if(!campground.author.equals(req.user._id)){
+		req.flash("error", "You do not have permission to do that!");
+		return res.redirect(`/campgrounds/${id}`);
+	}
+	next();
+}
+
 router.get("/", catchAsync(async function(req, res){
 	var campgrounds = await Campground.find({});
 	res.render("campgrounds/index", {campgrounds});
@@ -44,27 +54,18 @@ router.get("/:id", catchAsync(async function(req, res){
 	res.render("campgrounds/show", {campground});
 }));
 
-router.get("/:id/edit", isLoggedIn, catchAsync( async function(req, res){
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync( async function(req, res){
 	var {id} = req.params;
 	var campground = await Campground.findById(id)
 	if(!campground){
 		req.flash("error", "Cannot find that campground!");
 		return res.redirect("/campgrounds");
 	}
-	if(!campground.author.equals(req.user._id)){
-		req.flash("error", "You do not have permission to do that!");
-		return res.redirect(`/campgrounds/${id}`);
-	}
 	res.render("campgrounds/edit", {campground});
 }));
 
 router.put("/:id", isLoggedIn, validateCampground, catchAsync( async function(req, res){
 	var {id}= req.params;
-	var campground = await Campground.findById(id);
-	if(!campground.author.equals(req.user._id)){
-		req.flash("error", "You do not have permission to do that!");
-		return res.redirect(`/campgrounds/${id}`);
-	}
 	var camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});  //Using spread (...) operator here
 	req.flash("success", "Successfully updated campground!");
 	res.redirect(`/campgrounds/${campground._id}`);
