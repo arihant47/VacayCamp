c471e753b7fd820a668296c4d2f4c91aa2ba5c35
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-18 06:08:36 +0000

    Update isLoggedIn middleware

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ var Review = require("./models/review");
 
 module.exports.isLoggedIn = function(req, res, next){
 	if(!req.isAuthenticated()){
-		req.session.returnTo = req.originalUrl;
 		req.flash("error", "You must be signed in first!");
 		return res.redirect("/login");
 	}
