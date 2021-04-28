c0f532746c785587398ccda4b014a85a4ffcfb59
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-28 12:05:12 +0000

    Add map marker

--- public/JavaScripts/showPageMap.js
+++ public/JavaScripts/showPageMap.js
@@ -3,5 +3,9 @@ var map = new mapboxgl.Map({
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v11', // style URL
 	center: [-74.5, 40], // starting position [lng, lat]
-	zoom: 4 // starting zoom
+	zoom: 8 // starting zoom
 });
+
+new mapboxgl.Marker()
+	.setLngLat([-74.5, 40])
+	.addTo(map)
