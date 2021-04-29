d1d79389ee9ee309973a847afde20dd513f6f405
Arihant Kharai <kharaiarihant47@gmail.com>
2021-04-29 18:15:04 +0000

    Change start zoom to 10

--- public/JavaScripts/showPageMap.js
+++ public/JavaScripts/showPageMap.js
@@ -3,7 +3,7 @@ var map = new mapboxgl.Map({
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v11', // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
-	zoom: 8 // starting zoom
+	zoom: 10 // starting zoom
 });
 
 new mapboxgl.Marker()
