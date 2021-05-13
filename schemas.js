8ae6c810a89fc8870dd2ed93e743deab0bc08ccd
Arihant Kharai <kharaiarihant47@gmail.com>
2021-05-13 05:54:07 +0000

    Add escapeHTML extection

--- schemas.js
+++ schemas.js
@@ -1,5 +1,25 @@
 var Joi = require("joi");
 
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
 module.exports.campgroundSchema = Joi.object({
 	campground: Joi.object({
 	title: Joi.string().required(),
